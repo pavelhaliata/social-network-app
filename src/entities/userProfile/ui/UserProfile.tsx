b7f8752474b650c9c51cc61e79c7b6f316e4d91e
PavelH <nesoglasnyj.pavel@gmail.com>
2024-05-10 09:30:23 +0300

    refactor: style UserProfile

--- src/entities/userProfile/ui/UserProfile.tsx
+++ src/entities/userProfile/ui/UserProfile.tsx
@@ -4,7 +4,6 @@ import {
   useGetUserStatusQuery,
 } from "../service/userProfileApi.ts";
 import { Avatar, Flex, Image, Popover, Typography } from "antd";
-import { useEffect } from "react";
 import {
   FacebookOutlined,
   GithubOutlined,
@@ -22,30 +21,46 @@ const Text = Typography;
 export const UserProfile = ({}: Props) => {
   const { id: userId } = useParams();
   // const navigate = useNavigate();
-  const {
-    data: user,
-
-    isSuccess,
-  } = useGetUserProfileQuery(Number(userId));
+  const { data: user } = useGetUserProfileQuery(Number(userId));
   const { data: userStatus } = useGetUserStatusQuery(Number(userId));
-  console.log(userStatus);
-  useEffect(() => {
-    if (isSuccess) {
-    }
-  }, [isSuccess]);
+
+  // const socialIcons: { key: string; icon: React.ReactNode }[] = [
+  //   { key: "github", icon: <GithubOutlined className="text-2xl" /> },
+  //   { key: "facebook", icon: <FacebookOutlined className="text-2xl" /> },
+  //   { key: "instagram", icon: <InstagramOutlined className="text-2xl" /> },
+  //   { key: "twitter", icon: <TwitterOutlined className="text-2xl" /> },
+  //   { key: "youtube ", icon: <YoutubeOutlined className="text-2xl" /> },
+  // ];
+
+  // const getSocialIcon = (iconKey: string) => {
+  //   const obj = socialIcons.find((el) => el.key === iconKey);
+  //   if (obj) return obj.icon;
+  //   return false;
+  // };
+  // let key: SocialContacts = {};
+  // if (user?.contacts) {
+  //   const contacts = Object.entries(user.contacts);
+  //   contacts.map((i) => {
+  //     key = i[0];
+  //     getSocialIcon(key);
+  //   });
+  // }
 
   return (
     <Flex gap={50}>
       <div className="overflow-hidden">
-        <Image
-          src={user?.photos.large}
-          alt="user logo"
-          fallback={defaultUserLogo}
-          width={200}
-          height={200}
-          className="max-w-52 w-full object-cover rounded-lg"
-        />
-        <Avatar shape="square" size={64} icon={<UserOutlined />} />
+        {user?.photos.large ? (
+          <Image
+            src={user?.photos.large}
+            alt="user logo"
+            fallback={defaultUserLogo}
+            width={200}
+            height={200}
+            className="max-w-52 w-full object-cover rounded-lg"
+          />
+        ) : (
+          <Avatar shape="square" size={164} icon={<UserOutlined />} />
+        )}
         <div>
           <Text className="font-bold">Status: </Text>
           <Text>{userStatus}</Text>
@@ -53,11 +68,11 @@ export const UserProfile = ({}: Props) => {
       </div>
       <Flex gap={10} className="flex-col">
         <div>
-          <Text className="font-bold">Full Name: </Text>
+          <Text className="font-bold">Full Name:</Text>
           <Text>{user?.fullName}</Text>
         </div>
         <div>
-          <Text className="font-bold">About Me: </Text>
+          <Text className="font-bold">About Me:</Text>
           <Text>{user?.aboutMe}</Text>
         </div>
         <div>
@@ -66,64 +81,83 @@ export const UserProfile = ({}: Props) => {
         </div>
         <div>
           <Text className="font-bold">Social: </Text>
-          // TODO: сделать в виде mapa
+          {/*TODO: сделать map*/}
           <Flex className="gap-x-2">
-            <Popover
-              placement="bottomLeft"
-              title={"Git Hub"}
-              content={user?.contacts.github}
-            >
-              <a
-                href={user?.contacts.github}
-                target="_blank"
-                className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
+            {user?.contacts.github && (
+              <Popover
+                placement="bottomLeft"
+                title={"Git Hub"}
+                content={user?.contacts.github}
               >
-                {user?.contacts.github && (
+                <a
+                  href={user?.contacts.github}
+                  target="_blank"
+                  className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
+                >
                   <GithubOutlined className="text-2xl" />
-                )}
-              </a>
-            </Popover>
-            <Popover
-              placement="bottomLeft"
-              title={"Facebook"}
-              content={user?.contacts.facebook}
-            >
-              <a
-                href={user?.contacts.facebook}
-                target="_blank"
-                className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
+                </a>
+              </Popover>
+            )}
+            {user?.contacts.facebook && (
+              <Popover
+                placement="bottomLeft"
+                title={"Facebook"}
+                content={user?.contacts.facebook}
               >
-                <FacebookOutlined className="text-2xl" />
-              </a>
-            </Popover>
-
-            <a
-              href={user?.contacts.twitter}
-              target="_blank"
-              className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
-            >
-              {user?.contacts.twitter && (
-                <TwitterOutlined className="text-2xl" />
-              )}
-            </a>
-            <a
-              href={user?.contacts.instagram}
-              target="_blank"
-              className="p-1 rounded-md transition ease-in-out delay-100 hover:text-light-100 hover:bg-primary-500"
-            >
-              {user?.contacts.instagram && (
-                <InstagramOutlined className="text-2xl" />
-              )}
-            </a>
-            <a
-              href={user?.contacts.youtube}
-              target="_blank"
-              className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
-            >
-              {user?.contacts.youtube && (
-                <YoutubeOutlined className="text-2xl" />
-              )}
-            </a>
+                <a
+                  href={user?.contacts.facebook}
+                  target="_blank"
+                  className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
+                >
+                  <FacebookOutlined className="text-2xl" />
+                </a>
+              </Popover>
+            )}
+            {user?.contacts.twitter && (
+              <Popover
+                placement="bottomLeft"
+                title={"Twitter"}
+                content={user?.contacts.twitter}
+              >
+                <a
+                  href={user?.contacts.twitter}
+                  target="_blank"
+                  className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
+                >
+                  <TwitterOutlined className="text-2xl" />
+                </a>
+              </Popover>
+            )}
+            {user?.contacts.instagram && (
+              <Popover
+                placement="bottomLeft"
+                title={"Instagram"}
+                content={user?.contacts.instagram}
+              >
+                <a
+                  href={user?.contacts.instagram}
+                  target="_blank"
+                  className="p-1 rounded-md transition ease-in-out delay-100 hover:text-light-100 hover:bg-primary-500"
+                >
+                  <InstagramOutlined className="text-2xl" />
+                </a>
+              </Popover>
+            )}
+            {user?.contacts.youtube && (
+              <Popover
+                placement="bottomLeft"
+                title={"YouTube"}
+                content={user?.contacts.youtube}
+              >
+                <a
+                  href={user?.contacts.youtube}
+                  target="_blank"
+                  className="p-1 rounded-md transition ease-in-out delay-150 hover:text-light-100 hover:bg-primary-500"
+                >
+                  <YoutubeOutlined className="text-2xl" />
+                </a>
+              </Popover>
+            )}
           </Flex>
         </div>
       </Flex>
